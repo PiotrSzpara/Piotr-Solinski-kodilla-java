fe1f2cada30f287164aa552877ecad7f15bd4ac2
Piotr Szpara-Soliński <72048074+PiotrSzpara@users.noreply.github.com>
2020-10-09 23:04:50 +0200

    Calculator test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,35 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser =  new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        }else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator(20,10);
+
+        int addResult = calculator.addNumbers();
+        int subResult = calculator.subNumbers();
+
+        if (addResult == 30){
+            System.out.println("Add method is OK");
+        }else {
+            System.out.println("Add method ERROR!");
+        }
+
+        if (subResult == 10){
+            System.out.println("Subtract method is OK");
+        }else {
+            System.out.println("Subtract method ERROR!");
+        }
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/user/SimpleUser.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing.user;
+
+public class SimpleUser {
+    public String username;
+
+    public SimpleUser(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
