4a558ff6f234c1cfd6d5fa25ef2c065613c54bfa
Piotr Szpara-Soliński <72048074+PiotrSzpara@users.noreply.github.com>
2020-10-15 22:00:50 +0200

    WeatherForecastTestSuite 6.5 correction

--- kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/weather/stub/WeatherForecast.java
@@ -1,5 +1,7 @@
 package com.kodilla.testing.weather.stub;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,6 +9,7 @@ public class WeatherForecast {
     private Temperatures temperatures;
 
     public WeatherForecast(Temperatures temperatures) {
+
         this.temperatures = temperatures;
     }
     public Map<String, Double> calculateForecast() {
@@ -19,5 +22,37 @@ public class WeatherForecast {
             resultMap.put(temperature.getKey(), temperature.getValue() + 1.0);
         }
         return resultMap;
+    }
+    public double calculateAverageTemp(){
+        double a = 0;
+        Map<String, Double> temperatures = this.temperatures.getTemperatures();
+        for(Map.Entry<String, Double> temperature : temperatures.entrySet()) {
+            a += temperature.getValue();
+        }
+            return a/temperatures.size();
+
+    }
+    public double calculateMedianTemp(){
+        double m = 0;
+        ArrayList<Double> newTemp = new ArrayList<>();
+        Map<String, Double> temperatures = this.temperatures.getTemperatures();
+        for (Map.Entry<String, Double> temperature : temperatures.entrySet()) {
+
+            newTemp.add(temperature.getValue());
+
+        }
+        int size = newTemp.size();
+        for (int i = 0; i < size/2; i++) {
+            newTemp.remove(Collections.max(newTemp));
+            newTemp.remove(Collections.min(newTemp));
+
+        }
+        if (newTemp.size() == 2) {
+            m += ((newTemp.get(0) + newTemp.get(1)) / 2);
+        }
+        if (newTemp.size() == 1) {
+            m += newTemp.get(0);
+        }
+        return m;
     }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/weather/mock/WeatherForecastTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/weather/mock/WeatherForecastTestSuite.java
@@ -9,8 +9,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,26 +19,33 @@ public class WeatherForecastTestSuite {
 
     private static int testCounter = 0;
 
+    @Mock
+    private Temperatures temperaturesMock;
+
+    private static WeatherForecast weatherForecast;
+
     @BeforeEach
     public void beforeEveryTest() {
         testCounter++;
         System.out.println("Preparing to execute test #" + testCounter);
+
+        Map<String, Double> temperaturesMap = new HashMap<>();
+        temperaturesMap.put("Rzeszow", 25.5);
+        temperaturesMap.put("Krakow", 26.2);
+        temperaturesMap.put("Wroclaw", 24.9);
+        temperaturesMap.put("Warszawa", 25.2);
+        temperaturesMap.put("Gdansk", 26.7);
+        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
+        weatherForecast = new WeatherForecast(temperaturesMock);
+
+
     }
 
-    @Mock
-    private Temperatures temperaturesMock;
 
     @Test
     void testCalculateForecastWithMock() {
         //Given
-        Map<String, Double> temperaturesMap = new HashMap<>();
-        temperaturesMap.put("Rzeszow", 25.5);
-        temperaturesMap.put("Krakow", 26.2);
-        temperaturesMap.put("Wroclaw", 24.8);
-        temperaturesMap.put("Warszawa", 25.2);
-        temperaturesMap.put("Gdansk", 26.1);
-        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
-        WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
+
 
         //When
         int quantityOfSensors = weatherForecast.calculateForecast().size();
@@ -51,69 +56,23 @@ public class WeatherForecastTestSuite {
     @Test
     void testCalculateAverageTemperatureWithMock() {
         //Given
-        Map<String, Double> temperaturesMap = new HashMap<>();
-        temperaturesMap.put("Kielce", 28.5);
-        temperaturesMap.put("Wałbrzych", 22.5);
-        temperaturesMap.put("Wieliczka", 29.5);
-        temperaturesMap.put("Gorzów", 21.5);
-        temperaturesMap.put("Gorz", 0.0);
 
-        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
-        WeatherForecast weatherForecast1 = new WeatherForecast(temperaturesMock);
 
         //When
-        double a = 0;
-        for(Map.Entry<String, Double> temp : temperaturesMap.entrySet()){
-            a += temp.getValue();
+        double avTemp = weatherForecast.calculateAverageTemp();
 
-        }
-        double avTemp = a/weatherForecast1.calculateForecast().size();
-
-                //Then
-        Assertions.assertEquals(20.4 , avTemp);
+        //Then
+        Assertions.assertEquals(25.7 , avTemp);
     }
     @Test
     void testCalculateMedianTemperatureWithMock() {
         //Given
-        Map<String, Double> temperaturesMap = new HashMap<>();
-        temperaturesMap.put("Rzeszow", 25.5);
-        temperaturesMap.put("Krakow", 26.5);
-        temperaturesMap.put("Wroclaw", 24.5);
-        temperaturesMap.put("Warszawa", 27.5);
-        temperaturesMap.put("Gdansk", 23.5);
-        temperaturesMap.put("Kielce", 28.5);
-        temperaturesMap.put("Wałbrzych", 22.5);
-        temperaturesMap.put("Wieliczka", 29.5);
-        temperaturesMap.put("Gorzów", 21.5);
-        temperaturesMap.put("Gorz", 0.0);
-
-
-        when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
-        WeatherForecast weatherForecast2 = new WeatherForecast(temperaturesMock);
 
         //When
-        double m = 0;
-        ArrayList<Double> newTemp = new ArrayList<>();
-        for (Map.Entry<String, Double> temp : temperaturesMap.entrySet()) {
 
-            newTemp.add(temp.getValue());
-        }
-
-        for (int i = 0; i < newTemp.size() + 1; i++) {
-            newTemp.remove(Collections.max(newTemp));
-            newTemp.remove(Collections.min(newTemp));
-
-        }
-        if (newTemp.size() == 2) {
-            m += ((newTemp.get(0) + newTemp.get(1)) / 2);
-        }
-        if (newTemp.size() == 1) {
-            m += newTemp.get(0);
-        }
-        double medTemp = m;
-        weatherForecast2.calculateForecast().size();
+        double medTemp = weatherForecast.calculateMedianTemp();
 
         //Then
-        Assertions.assertEquals(25.0 ,medTemp);
+        Assertions.assertEquals(25.5 ,medTemp);
     }
 }
