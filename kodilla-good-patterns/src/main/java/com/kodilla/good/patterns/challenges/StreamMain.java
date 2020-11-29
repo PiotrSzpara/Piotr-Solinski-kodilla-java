4ff5fd5d08448c78a78d61165c00e2a1feed33c2
Piotr Szpara-Soliński <72048074+PiotrSzpara@users.noreply.github.com>
2020-11-30 00:22:32 +0100

    MovieStore 13.1

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/StreamMain.java
@@ -0,0 +1,24 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+        Map<String, List<String>> movieMap = movieStore.getMovies();
+
+        movieMap.forEach((key, value) -> System.out.println(key + " ! " + String.join(" ! ", value)));
+
+        final String movies = movieMap.entrySet()
+                .stream().
+                flatMap(e -> e.getValue().stream()).
+                collect(Collectors.joining(" ! "));
+        System.out.println(movies);
+
+
+
+    }
+}
