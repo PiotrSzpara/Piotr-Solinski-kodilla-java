39e28cd03d69b3a960a30a78a0f3e0518c870461
Piotr Szpara-Soliński <72048074+PiotrSzpara@users.noreply.github.com>
2020-12-07 00:11:50 +0100

    bean ForumUser 14.1

---
+++ kodilla-spring-main/src/main/java/com/kodilla/KodillaSpringMainApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaSpringMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodillaSpringMainApplication.class, args);
+	}
+
+}
